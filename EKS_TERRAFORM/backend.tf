b095c6e5becd98a503b9b2f0b0299ff69f255594
Sushant Sonbarse <sushantsonbarse90@gmail.com>
2025-04-08 10:49:15 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "sushant-tf-state-123" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-west-2"
   }
 }
