c98465826c8aed2f99c72435c90d131b0fee68a4
Sushant Sonbarse <sushantsonbarse90@gmail.com>
2025-04-10 14:33:38 +0530

    Update variables.tf

--- EKS_TERRAFORM/variables.tf
+++ EKS_TERRAFORM/variables.tf
@@ -1,7 +1,7 @@
 variable "access_key" {
-  default = "AKIAQGYBQC554HOPGFFV"                # aws access key
+  default = ""                # aws access key
 }
 
 variable "secret_key" {
-  default = "v4ncqNFWKv7KTlcFGgVhxTonLvB0JTgKkba65Na2"         # aws secret key
+  default = ""         # aws secret key
 }
